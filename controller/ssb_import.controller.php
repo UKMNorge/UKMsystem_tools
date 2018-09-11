7987f95cb8000eee7945ba1ca156efe97fd690e6
Marius Mandal <mariusmandal@gmail.com>
2018-09-11 12:40:58 +0200

    kommuneAreal

--- controller/ssb_import.controller.php
+++ controller/ssb_import.controller.php
@@ -2,22 +2,9 @@
 ### LEVENDEFØDTE - OOPS, denne fyller opp generisk namespace med funksjoner og stuff
 require_once(__DIR__.'/SSB/levendefodte.controller.php');
 ### AREAL AV KOMMUNER:
-#require_once(__DIR__.'/SSB/areal.controller.php');
+require_once(__DIR__.'/SSB/areal.controller.php');
 ### KOMMUNER I NORGE:
-#require_once(__DIR__.'/SSB/kommuneliste.controller.php');
-
-
-### LEVENDEFØDTE:
-#$TWIGdata = checkUpdate($TWIGdata);
-
-#$levendefodte = new stdClass();
-#$levendefodte->last_updated = get_latest_year_updated();
-#$levendefodte->missing_years = get_missing_years();
+require_once(__DIR__.'/SSB/kommuneliste.controller.php');
 
 $TWIGdata['levendefodte'] = $levendefodte;
-
-#$areal = new stdClass();
-#$areal->last_updated = get_latest_year_updated();
-#$areal->missing_years = get_missing_years();
-
-#$TWIGdata['areal'] = $areal;
+$TWIGdata['areal'] = $kommuneareal;
