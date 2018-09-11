2a0b672f2b18fd03efe536a24386f9168339aebb
Marius Mandal <mariusmandal@gmail.com>
2018-09-11 12:12:13 +0200

    Refactor levendefodte

--- controller/ssb_import.controller.php
+++ controller/ssb_import.controller.php
@@ -2,16 +2,22 @@
 ### LEVENDEFØDTE - OOPS, denne fyller opp generisk namespace med funksjoner og stuff
 require_once(__DIR__.'/SSB/levendefodte.controller.php');
 ### AREAL AV KOMMUNER:
-require_once(__DIR__.'/SSB/areal.controller.php');
+#require_once(__DIR__.'/SSB/areal.controller.php');
 ### KOMMUNER I NORGE:
-require_once(__DIR__.'/SSB/kommuneliste.controller.php');
+#require_once(__DIR__.'/SSB/kommuneliste.controller.php');
 
 
 ### LEVENDEFØDTE:
-$TWIGdata = checkUpdate($TWIGdata);
+#$TWIGdata = checkUpdate($TWIGdata);
 
-$levendefodte = new stdClass();
-$levendefodte->last_updated = get_latest_year_updated();
-$levendefodte->missing_years = get_missing_years();
+#$levendefodte = new stdClass();
+#$levendefodte->last_updated = get_latest_year_updated();
+#$levendefodte->missing_years = get_missing_years();
 
 $TWIGdata['levendefodte'] = $levendefodte;
+
+#$areal = new stdClass();
+#$areal->last_updated = get_latest_year_updated();
+#$areal->missing_years = get_missing_years();
+
+#$TWIGdata['areal'] = $areal;
