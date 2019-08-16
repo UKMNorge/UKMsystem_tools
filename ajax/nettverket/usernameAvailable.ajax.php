5b0c666dd79c571be566d874e46409ebc2490c16
Marius Mandal <mariusmandal@gmail.com>
2019-08-16 12:44:48 +0200

    extends UKMWPmodul :tada:

--- ajax/nettverket/usernameAvailable.ajax.php
+++ ajax/nettverket/usernameAvailable.ajax.php
@@ -1,12 +1,13 @@
 <?php
 
 use UKMNorge\Wordpress\User;
+header('Content-Type: application/json');
 
 require_once('UKM/Wordpress/User.class.php');
 
 $success = User::isAvailableUsername( $_POST['username'] );
 
-echo json_encode(
+UKMsystem_tools::addResponseData(
     [
         'success' => $success,
         'message' => 'Brukernavnet er '. ($success ? 'ledig' : 'allerede i bruk'),
