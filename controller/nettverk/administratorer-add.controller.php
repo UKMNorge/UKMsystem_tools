98c0554ce4366ae0469786725c01f42284c302ca
Marius Mandal <mariusmandal@gmail.com>
2019-08-16 14:51:56 +0200

    Oppretting av brukere fungerer

--- controller/nettverk/administratorer-add.controller.php
+++ controller/nettverk/administratorer-add.controller.php
@@ -1,60 +1,40 @@
 <?php
 
-require_once('UKM/fylker.class.php');
-UKMsystem_tools::addViewData(
-    'fylke',
-    fylker::getById($_GET['fylke'])
-);
+use UKMNorge\Wordpress\User;
 
-// DEBUG
+require_once('UKM/fylker.class.php');
+require_once('UKM/Wordpress/User.class.php');
+require_once('UKM/Wordpress/WriteUser.class.php');
+
+UKMsystem_tools::addViewData('fylke', fylker::getById($_GET['fylke']));
+
+/* BRUKEREN HAR SKREVET INN E-POSTADRESSE */
 if (true || $_SERVER['REQUEST_METHOD'] == 'POST') {
     // DEBUG
-    #$_POST['email'] = 'testbruker@ukm.dev';
-    $_POST['email'] = 'mariusmandal@gmail.com';
-    $_POST['email'] = 'testkommune@gmail.com';
+    $_POST['email'] = 'testbruker@ukm.dev'; # Ledig bruker
+    $_POST['email'] = 'mariusmandal@gmail.com'; # Eksisterende bruker
+    #$_POST['email'] = 'ukm norge@gmail.com'; # Ledig bruker, opptatt brukernavn
 
-    $user = get_user_by('email', $_POST['email']);
 
-    if (isset($_POST['user_id'])) {
-        if ($_POST['user_id'] == 'new' && username_exists($_POST['username'])) {
-            UKMsystem_tools::getFlash()->add(
-                'danger',
-                'Brukernavnet er opptatt. Prøv et annet.'
-            );
-        }
-        $user->first_name = $_POST['first_name'];
-        $user->last_name = $_POST['last_name'];
-    } else {
-        UKMsystem_tools::addViewData(
-            'doAdd',
-            true
-        );
-        if ($user) {
-            $user->first_name = get_user_meta($user->ID, 'first_name', true);
-            $user->last_name = get_user_meta($user->ID, 'last_name', true);
-            $user->phone = get_user_meta($user->ID, 'phone', true);
-            $user->email = $user->data->user_email;
-            $user->username = $user->data->user_login;
-        } else {
-            $user = new stdClass();
-            $user->ID = 'new';
-            $user->first_name = '';
-            $user->last_name = '';
-            $user->email = $_POST['email'];
-            $user->username = substr(
+    // Hent bruker eller opprett placeholder-objekt
+    try {
+        $user = User::loadByEmail($_POST['email']);
+    } catch (Exception $e) {
+        $user = User::createEmpty();
+    }
+
+    UKMsystem_tools::addViewData('doAdd',true);
+
+    // Bruker eksisterer ikke - fyll ut e-post og brukernavn
+    if (!$user->isReal()) {
+        $user->setEmail($_POST['email']);
+        $user->setUsername(
+            substr(
                 $_POST['email'],
                 0,
                 strpos($_POST['email'], '@')
-            );
-
-            UKMsystem_tools::addViewData(
-                'username_taken',
-                username_exists($user->username)
-            );
-        }
+            )
+        );
     }
-    UKMsystem_tools::addViewData(
-        'user',
-        $user
-    );
+    UKMsystem_tools::addViewData('user',$user);
 }
--- controller/nettverk/administratorer.controller.php
+++ controller/nettverk/administratorer.controller.php
@@ -1,5 +1,39 @@
 <?php
 
+use UKMNorge\Wordpress\User;
+use UKMNorge\Wordpress\WriteUser;
+
 require_once('UKM/fylker.class.php');
+require_once('UKM/Wordpress/User.class.php');
+require_once('UKM/Wordpress/WriteUser.class.php');
 
 UKMsystem_tools::addViewData('fylker', fylker::getAll());
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
+        $user = User::loadByEmail($_POST['email']);
+    } catch (Exception $e) {
+        $user = User::createEmpty();
+    }
+
+    // OPPRETT + OPPDATER BRUKER
+    if (isset($_POST['user_id'])) {
+        // Opprett bruker
+        if ($_POST['user_id'] == 'new') {
+            $user->setEmail($_POST['email']);
+            $user->setUsername($_POST['username']);
+        }
+        $user->setFirstName($_POST['first_name']);
+        $user->setLastName($_POST['last_name']);
+        $user->setPhone((int) $_POST['phone']);
+
+        WriteUser::save($user);
+
+        $fylke = Fylker::getById($_POST['fylke_id']);
+
+        UKMsystem_tools::getFlash()->add(
+            'info',
+            $user->getName() . ' er lagt til som bruker (og etter hvert administrator for ' . $fylke->getNavn() . ')'
+        );
+    }
+}
